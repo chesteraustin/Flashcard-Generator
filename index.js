3a8450bf1dda2bd2e699dbc8979e085a452ad5f1
chesteraustin <chesteraustin@gmail.com>
2017-06-29 08:56:06 -0700

    test with initial examples

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+(function(){
+	var BasicCard = require("./BasicCard");
+	var ClozeCard = require("./ClozeCard");
+/*
+console.log(BasicCard);
+var firstPresident = new BasicCard("Who was the first president of the United States?", "George Washington");
+
+// "Who was the first president of the United States?"
+console.log(firstPresident.front); 
+
+// "George Washington"
+console.log(firstPresident.back); 
+*/
+var firstPresidentCloze = new ClozeCard(
+    "George Washington was the first president of the United States.", "George Washington");
+// "George Washington was the first president of the United States."
+console.log(firstPresidentCloze.fullText); 
+// "George Washington"
+console.log(firstPresidentCloze.cloze); 
+
+// " ... was the first president of the United States.
+console.log(firstPresidentCloze.partial()); 
+// "George Washington was the first president of the United States."
+
+console.log(firstPresidentCloze.fullText);
+
+// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+var brokenCloze = new ClozeCard("This doesn't work", "oops");
+console.log(brokenCloze);
+/*
+
+*/
+})();
